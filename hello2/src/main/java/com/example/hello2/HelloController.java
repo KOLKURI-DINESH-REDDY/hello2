9969632d715cc857230d20b76346f1f905eed9c8
dinesh <kolkuridineshreddy@gmail.com>
2025-03-10 14:58:48 +0530

    v1

---
+++ hello2/src/main/java/com/example/hello2/HelloController.java
@@ -0,0 +1,17 @@
+
+package com.example.hello2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+    @GetMapping("hii")
+    public String hii(){
+        return "hey how are yous";
+    }
+    @GetMapping("hello")
+    public String hello()
+    {
+        return "hello";
+    }
+}
