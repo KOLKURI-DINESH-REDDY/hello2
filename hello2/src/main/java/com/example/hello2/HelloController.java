7f0535523151f1e60ddf5277706c46fd5562e88c
dinesh <kolkuridineshreddy@gmail.com>
2025-03-10 16:36:50 +0530

    v4

--- hello2/src/main/java/com/example/hello2/HelloController.java
+++ hello2/src/main/java/com/example/hello2/HelloController.java
@@ -5,10 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
-    @GetMapping("hii")
-    public String hii(){
-        return "hey how are yous";
-    }
+    
     @GetMapping("hello")
     public String hello()
     {
